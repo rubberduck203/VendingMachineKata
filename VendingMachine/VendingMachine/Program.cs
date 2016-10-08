7ff1e8a0cb98ddad435989353946ac81b25fc743
Chris McClellan <ckuhn203@gmail.com>
2016-10-08 12:05:25 -0400

    The PO wanted to see the code run for the demo.
    Created throw away console app for demo.

--- VendingMachine/VendingMachine/Program.cs
+++ VendingMachine/VendingMachine/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vending.Core;
 
 namespace Vending
 {
@@ -10,6 +11,28 @@ namespace Vending
     {
         static void Main(string[] args)
         {
+            // We'll use this simple console app to demo until the hardware is ready
+
+            Console.WriteLine("Vending Machine!");
+            Console.WriteLine();
+            Console.WriteLine("[1] - Nickel");
+            Console.WriteLine("[2] - Dime");
+            Console.WriteLine("[3] - Quarter");
+            Console.WriteLine();
+
+            var vendingMachine = new VendingMachine();
+
+            Console.WriteLine(vendingMachine.GetDisplayText());
+
+            while (true)
+            {
+                int input;
+                if (Int32.TryParse(Console.ReadLine(), out input))
+                {
+                    vendingMachine.Accept((Coin)input);
+                    Console.WriteLine(vendingMachine.GetDisplayText());
+                }
+            }
         }
     }
 }
