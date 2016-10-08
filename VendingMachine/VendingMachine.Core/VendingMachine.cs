171c8c6c5718fa3aa822e7c878808a8195a5b83e
Chris McClellan <ckuhn203@gmail.com>
2016-10-08 14:01:34 -0400

    Moved CurrentValueState to its own file.

--- VendingMachine/VendingMachine.Core/VendingMachine.cs
+++ VendingMachine/VendingMachine.Core/VendingMachine.cs
@@ -51,47 +51,4 @@ namespace Vending.Core
             return _machineState.Display();
         }
     }
-
-    public class CurrentValueState : VendingMachineState
-    {
-        private readonly IEnumerable<Coin> _coins;
-
-        public CurrentValueState(IEnumerable<Coin> coins)
-        {
-            _coins = coins;
-        }
-
-        public string Display()
-        {
-            return $"{CurrentTotal(_coins):C}";
-        }
-
-        private static decimal CurrentTotal(IEnumerable<Coin> coins)
-        {
-            var counts = new Dictionary<Coin, int>()
-            {
-                {Coin.Nickel, 0},
-                {Coin.Dime, 0},
-                {Coin.Quarter, 0}
-            };
-
-            foreach (var coin in coins)
-            {
-                counts[coin]++;
-            }
-
-            decimal total = 0;
-            foreach (var coinCount in counts)
-            {
-                total += (coinCount.Value * coinCount.Key.Value());
-            }
-
-            return ConvertCentsToDollars(total);
-        }
-
-        private static decimal ConvertCentsToDollars(decimal total)
-        {
-            return total / 100;
-        }
-    }
 }
