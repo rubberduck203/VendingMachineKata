4d0c77b26bcf4e5ab2ee1ed14e4039740f9fcf71
Chris McClellan <ckuhn203@gmail.com>
2016-10-08 13:24:24 -0400

    Added PriceState class

---
+++ VendingMachine/VendingMachine.Tests.Core/PriceStateTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vending.Tests.Core
+{
+    [TestClass]
+    public class PriceStateTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
