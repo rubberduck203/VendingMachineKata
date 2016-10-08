25fd562fb2070894e5bd7042e8ce28d3d0eab56c
Chris McClellan <ckuhn203@gmail.com>
2016-10-08 08:51:59 -0400

    Extracted test fixture

--- VendingMachine/VendingMachine.Tests.Core/VendingMachineTests.cs
+++ VendingMachine/VendingMachine.Tests.Core/VendingMachineTests.cs
@@ -7,20 +7,27 @@ namespace Vending.Tests.Core
     [TestClass]
     public class VendingMachineTests
     {
+        private VendingMachine _vendingMachine;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _vendingMachine = new VendingMachine();
+        }
+
         [TestMethod]
         public void VendingMachine_OnStartUp_Displays_InsertCoin()
         {
-            var vendingMachine = new VendingMachine();
-            Assert.AreEqual("INSERT COIN", vendingMachine.GetDisplayText());
+            Assert.AreEqual("INSERT COIN", _vendingMachine.GetDisplayText());
         }
 
         [TestMethod]
         public void VendingMachine_GivenANickel_Displays_5()
         {
-            var vendingMachine = new VendingMachine();
-            vendingMachine.Accept(Coin.Nickel);
-
-            Assert.AreEqual("5", vendingMachine.GetDisplayText());
+            _vendingMachine.Accept(Coin.Nickel);
+            Assert.AreEqual("5", _vendingMachine.GetDisplayText());
         }
+
+
     }
 }
