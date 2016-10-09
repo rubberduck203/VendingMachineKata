5fb815d45429ce5918a06b6cde8c575b58a77008
Chris McClellan <ckuhn203@gmail.com>
2016-10-09 13:48:56 -0400

    Extracted ReduceNickel method

--- VendingMachine/VendingMachine.Core/RefundCalculator.cs
+++ VendingMachine/VendingMachine.Core/RefundCalculator.cs
@@ -6,13 +6,6 @@ namespace Vending.Core
     {
         public IDictionary<Coin, int> CalculateRefund(int priceInCents, int paidInCents)
         {
-            /* Note: I'm not particularly fond of the implementation here.
-             * I don't care for the duplication, or the side effects.
-             * I suspect there's a more functional way to do this.
-             * That said, the implementation is exceedingly unlikely to 
-             * change in the future, so goood enough is good enough.
-             */
-
             var refundValue = paidInCents - priceInCents;
 
             if (refundValue <= 0)
@@ -27,22 +20,13 @@ namespace Vending.Core
 
             var nickels = refundValue / Coin.Nickel.Value();
 
-            var dimes = 0;
-            var quarters = 0;
-
             const int nickelsPerQuarter = 5;
-            if (nickels >= nickelsPerQuarter)
-            {
-                quarters = nickels / nickelsPerQuarter;
-                nickels -= (quarters * nickelsPerQuarter);
-            }
+            int quarters;
+            nickels = ReduceNickels(nickels, nickelsPerQuarter, out quarters);
 
             const int nickelsPerDime = 2;
-            if (nickels >= nickelsPerDime)
-            {
-                dimes = nickels / nickelsPerDime;
-                nickels -= (dimes * nickelsPerDime);
-            }
+            int dimes;
+            nickels = ReduceNickels(nickels, nickelsPerDime, out dimes);
 
             return new Dictionary<Coin, int>()
             {
@@ -50,6 +34,17 @@ namespace Vending.Core
                 {Coin.Dime, dimes},
                 {Coin.Quarter, quarters}
             };
+        }
+
+        private static int ReduceNickels(int nickels, int nickelsPerCoin, out int coinCount)
+        {
+            coinCount = 0;
+            if (nickels >= nickelsPerCoin)
+            {
+                coinCount = nickels / nickelsPerCoin;
+                nickels -= (coinCount * nickelsPerCoin);
+            }
+            return nickels;
         }
     }
 }
