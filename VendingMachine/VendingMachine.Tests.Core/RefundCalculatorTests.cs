0950f8f8d3b846ca31827924e1226e60f6f7a300
Chris McClellan <ckuhn203@gmail.com>
2016-10-09 11:46:32 -0400

    Extracted TestInitialize method

--- VendingMachine/VendingMachine.Tests.Core/RefundCalculatorTests.cs
+++ VendingMachine/VendingMachine.Tests.Core/RefundCalculatorTests.cs
@@ -8,39 +8,39 @@ namespace Vending.Tests.Core
     [TestClass]
     public class RefundCalculatorTests
     {
+        private RefundCalculator _refunder;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _refunder = new RefundCalculator();
+        }
+
         [TestMethod]
         public void RefundCalculator_ZeroBalance_ReturnsZero()
         {
-            var refunder = new RefundCalculator();
-            var actual = refunder.CalculateRefund(50, 50);
-
+            var actual = _refunder.CalculateRefund(50, 50);
             AssertRefund(actual, nickels: 0, dimes:0, quarters:0);
         }
 
         [TestMethod]
         public void RefundCalculator_Price70_Given75_RefundANickel()
         {
-            var refunder = new RefundCalculator();
-            var actual = refunder.CalculateRefund(70, 75);
-
+            var actual = _refunder.CalculateRefund(70, 75);
             AssertRefund(actual, nickels:1, dimes:0, quarters:0);
         }
 
         [TestMethod]
         public void RefundCalculator_Price65_Given75_RefundADime()
         {
-            var refunder = new RefundCalculator();
-            var actual = refunder.CalculateRefund(65, 75);
-
+            var actual = _refunder.CalculateRefund(65, 75);
             AssertRefund(actual, nickels: 0, dimes: 1, quarters: 0);
         }
 
         [TestMethod]
         public void RefundCalculator_Price60_Given75_RefundADimeAndNickel()
         {
-            var refunder = new RefundCalculator();
-            var actual = refunder.CalculateRefund(60, 75);
-
+            var actual = _refunder.CalculateRefund(60, 75);
             AssertRefund(actual, nickels:1, dimes:1, quarters:0);
         }
 
