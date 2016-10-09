bede5910748fea6a652a2e51f0f790a92f8f2211
Chris McClellan <ckuhn203@gmail.com>
2016-10-09 11:20:14 -0400

    Zero balance refund calculated

--- VendingMachine/VendingMachine.Core/RefundCalculator.cs
+++ VendingMachine/VendingMachine.Core/RefundCalculator.cs
@@ -1,6 +1,17 @@
-﻿namespace Vending.Core
+﻿using System.Collections.Generic;
+
+namespace Vending.Core
 {
     public class RefundCalculator
     {
+        public IDictionary<Coin, int> CalculateRefund(int priceInCents, int paidInCents)
+        {
+            return new Dictionary<Coin, int>()
+            {
+                {Coin.Nickel, 0},
+                {Coin.Dime, 0},
+                {Coin.Quarter, 0}
+            };
+        }
     }
 }
--- VendingMachine/VendingMachine.Tests.Core/RefundCalculatorTests.cs
+++ VendingMachine/VendingMachine.Tests.Core/RefundCalculatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Vending.Core;
 
@@ -11,6 +12,11 @@ namespace Vending.Tests.Core
         public void RefundCalculator_ZeroBalance_ReturnsZero()
         {
             var refunder = new RefundCalculator();
+            var actual = refunder.CalculateRefund(50, 50);
+
+            Assert.AreEqual(0, actual[Coin.Nickel]);
+            Assert.AreEqual(0, actual[Coin.Dime]);
+            Assert.AreEqual(0, actual[Coin.Quarter]);
         }
     }
 }
