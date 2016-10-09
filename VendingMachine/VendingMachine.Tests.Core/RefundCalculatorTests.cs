02ac2482dcc2b6c1ca43d290d3b75a7db80a7046
Chris McClellan <ckuhn203@gmail.com>
2016-10-09 12:01:09 -0400

    Added characterization test for 30 cent refund

--- VendingMachine/VendingMachine.Tests.Core/RefundCalculatorTests.cs
+++ VendingMachine/VendingMachine.Tests.Core/RefundCalculatorTests.cs
@@ -58,6 +58,16 @@ namespace Vending.Tests.Core
             AssertRefund(actual, nickels:0, dimes:0, quarters:1);
         }
 
+        [TestMethod]
+        public void RefundCalculator_Price50_Given80_Refund3Dimes()
+        {
+            var acutal = _refunder.CalculateRefund(50, 80);
+            AssertRefund(acutal, nickels:0, dimes:3, quarters:0);
+
+            /* The test above is acceptable, but the one below would be better. */
+            //AssertRefund(acutal, nickels:1, dimes:0, quarters:1);
+        }
+
         private static void AssertRefund(IDictionary<Coin, int> actual, int nickels, int dimes, int quarters)
         {
             Assert.AreEqual(nickels, actual[Coin.Nickel]);
