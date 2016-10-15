d0df534b53292129d314549d2a96067a05466f04
Chris McClellan <ckuhn203@gmail.com>
2016-10-15 11:37:10 -0400

    Extracted test fixture

--- VendingMachine/VendingMachine.Tests.Core/SoldOutTests.cs
+++ VendingMachine/VendingMachine.Tests.Core/SoldOutTests.cs
@@ -7,29 +7,33 @@ namespace Vending.Tests.Core
     [TestClass]
     public class SoldOutTests
     {
+        private ProductInfoRepository _fakeProductInfoRepository;
+        private VendingMachine _vendingMachine;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _fakeProductInfoRepository = new FakeProductInfoRepository() { QuantityAvailable = 0, Price = 65 };
+            _vendingMachine = new VendingMachine(_fakeProductInfoRepository);
+        }
+
         [TestMethod]
         public void VendingMachine_WhenSoldOutAndNoMoneyInserted_DisplaysSoldOutThenInsertCoin()
         {
-            var fakeProductInfoRepository = new FakeProductInfoRepository() { QuantityAvailable = 0 };
-            var vendingMachine = new VendingMachine(fakeProductInfoRepository);
+            _vendingMachine.Dispense("candy");
 
-            vendingMachine.Dispense("candy");
-
-            Assert.AreEqual("SOLD OUT", vendingMachine.GetDisplayText());
-            Assert.AreEqual("INSERT COIN", vendingMachine.GetDisplayText());
+            Assert.AreEqual("SOLD OUT", _vendingMachine.GetDisplayText());
+            Assert.AreEqual("INSERT COIN", _vendingMachine.GetDisplayText());
         }
 
         [TestMethod]
         public void VendingMachine_WhenSoldOutAndMoneyIsInMachine_DisplaySoldOutThenCurrentAmount()
         {
-            var fakeProductInfoRepository = new FakeProductInfoRepository() { QuantityAvailable = 0, Price = 65 };
-            var vendingMachine = new VendingMachine(fakeProductInfoRepository);
+            _vendingMachine.Accept(Coin.Quarter);
+            _vendingMachine.Dispense("candy");
 
-            vendingMachine.Accept(Coin.Quarter);
-            vendingMachine.Dispense("candy");
-
-            Assert.AreEqual("SOLD OUT", vendingMachine.GetDisplayText());
-            Assert.AreEqual("$0.25", vendingMachine.GetDisplayText());
+            Assert.AreEqual("SOLD OUT", _vendingMachine.GetDisplayText());
+            Assert.AreEqual("$0.25", _vendingMachine.GetDisplayText());
         }
     }
 }
