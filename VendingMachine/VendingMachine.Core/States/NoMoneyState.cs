00e65ecfd8f8b51f57fd09ad44ac4f78edcdce77
Chris McClellan <ckuhn203@gmail.com>
2016-10-15 08:46:44 -0400

    NoMoney is returned when there's no money to return

--- VendingMachine/VendingMachine.Core/States/NoMoneyState.cs
+++ VendingMachine/VendingMachine.Core/States/NoMoneyState.cs
@@ -1,10 +1,20 @@
-﻿namespace Vending.Core.States
+﻿using System.Collections.Generic;
+
+namespace Vending.Core.States
 {
     public class NoMoneyState : VendingMachineState
     {
+        private readonly List<Coin> _returnTray = new List<Coin>();
+        public IEnumerable<Coin> ReturnTray => _returnTray;
+
         public override string Display()
         {
             return "INSERT COIN";
+        }
+
+        public void Refund()
+        {
+            
         }
     }
 }
---
+++ VendingMachine/VendingMachine.Tests.Core/NoMoneyStateTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vending.Core;
+using Vending.Core.States;
+
+namespace Vending.Tests.Core
+{
+    [TestClass]
+    public class NoMoneyStateTests
+    {
+        [TestMethod]
+        public void NoMoneyState_WhenRefund_NoMoneyRefunded()
+        {
+            var state = new NoMoneyState();
+            state.Refund();
+
+            CollectionAssert.AreEqual(new List<Coin>(), state.ReturnTray.ToList());
+        }
+    }
+}
