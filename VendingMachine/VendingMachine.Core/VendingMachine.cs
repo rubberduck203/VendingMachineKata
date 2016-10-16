abcca6fdfe58fae7bc85ce3759e58342c8cd16a4
Chris McClellan <ckuhn203@gmail.com>
2016-10-16 09:00:07 -0400

    Moved StateContext interface to its own file

--- VendingMachine/VendingMachine.Core/VendingMachine.cs
+++ VendingMachine/VendingMachine.Core/VendingMachine.cs
@@ -4,11 +4,6 @@ using Vending.Core.States;
 
 namespace Vending.Core
 {
-    public interface StateContext
-    {
-        VendingMachineState State { get; set; }
-    }
-
     public class VendingMachine : StateContext
     {
         public VendingMachine(ProductInfoRepository productInfoRepository)
